000d269d55f166c6209ef960c200ff09ee1d1d53
imsoobin <imsoobin@localhost>
2021-05-27 16:33:07 +0900

    SQL

--- WebContent/WEB-INF/sql/SELECT01.sql
+++ WebContent/WEB-INF/sql/SELECT01.sql
@@ -10,6 +10,15 @@ SELECT CustomerName FROM Customers;
 -- 한 줄 주석
 SELECT City FROM Customers;
 
+-- 데이블 조회
+SELECT * FROM Categories; -- 8
+SELECT * FROM Customers; -- 91
+SELECT * FROM Employees; -- 10
+SELECT * FROM OrderDetails; -- 518
+SELECT * FROM Orders; -- 196
+SELECT * FROM Products; -- 7
+SELECT * FROM Shippers; -- 3
+
 
 
 
---
+++ WebContent/WEB-INF/sql/SELECT03DISTINCT.sql
@@ -0,0 +1,20 @@
+SELECT City FROM Customers;
+
+SELECT DISTINCT City FROM Customers;
+
+-- COUNT Function  몇개 있는지 알려줌 
+SELECT COUNT(*) FROM Customers;
+
+SELECT COUNT(City) FROM Customers;
+
+SELECT DISTINCT Country FROM Customers;
+
+SELECT DISTINCT City,Country FROM Customers;
+
+
+
+
+
+
+
+
