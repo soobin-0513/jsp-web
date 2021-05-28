1b57b1efb23e74db1809b4fb4e733f54cc73f399
imsoobin <imsoobin@172.30.1.24>
2021-05-28 17:26:55 +0900

    servlet

--- src/ch14/JDBC11Servlet.java
+++ src/ch14/JDBC11Servlet.java
@@ -13,27 +13,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import ch14.bean.Employee;
 
-/**
- * Servlet implementation class JDBC11Servlet
- */
 @WebServlet("/JDBC11Servlet")
 public class JDBC11Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	
     public JDBC11Servlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String eid = request.getParameter("eid");
 		
 		Employee employee = executeJDBC(eid);
@@ -42,55 +34,66 @@ public class JDBC11Servlet extends HttpServlet {
 		
 		String path = "/ch14/jdbc11.jsp";
 		request.getRequestDispatcher(path).forward(request, response);
+		
 	}
 	
 	private Employee executeJDBC(String eid) {
 
-		Employee employee = null; // 리턴할 객체
-		
-		String sql = "SELECT Employees,EmployeeID FROM Customers " ;
+		// Return 할 객체
+		Employee employee = null;
 
-		String url ="jdbc:mysql://52.79.195.216/test";
+		// SQL
+		String sql = "SELECT EmployeeID, LastName, FirstName " + 
+				"FROM Employees WHERE EmployeeID = " + eid;
+
+		String url = "jdbc:mysql://52.79.195.216/test";
 		String user = "root";
 		String password = "wnddkdwjdqhcjfl1";
 
 		Connection con = null;
 		Statement stmt = null;
-		ResultSet rs = null;
+		ResultSet rs = null; 
 
 		try {
-			// 클래스 로딩
+			// 클래스 로딩 (class loading)
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			// 연결하기/ !!
+			// 연결
 			con = DriverManager.getConnection(url, user, password);
 
 			// statement 생성
 			stmt = con.createStatement();
 
-			// 쿼리 실행, 결과(ResultSet) 리턴
+			// query 실행, 결과(ResultSet) 리턴
 			rs = stmt.executeQuery(sql);
 
 			// 결과 탐색
 			if (rs.next()) {
-				String lastname = rs.getString(1);
-				String firstname = rs.getString(2);
+				int id = rs.getInt(1);
+				String lastName = rs.getNString(2);
+				String firstName = rs.getString(3);
 				
 				
-				employee = new Employee();
-				employee.setLastname(lastname);
-				employee.setFirstname(firstname);
+				// Customer 객체 생성
+				employee  = new Employee();
+				
+				employee.setId(id);
+				employee.setLastname(lastName);
+				employee.setFirstname(firstName);
+				
+				
 			}
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
+
 		} finally {
 			// 연결 닫기
 			if (rs != null) {
 				try {
 					rs.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -99,7 +102,6 @@ public class JDBC11Servlet extends HttpServlet {
 				try {
 					stmt.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -108,21 +110,16 @@ public class JDBC11Servlet extends HttpServlet {
 				try {
 					con.close();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
-
+		
 		return employee;
-
 	}
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
