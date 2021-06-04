11a2290f23ed20ec71a582c000b9d5ca3165a766
imsoobin <imsoobin@localhost>
2021-06-04 17:51:13 +0900

    sql

---
+++ WebContent/WEB-INF/sql/TABLE02(member&board).sql
@@ -0,0 +1,21 @@
+USE test2;
+
+SELECT * FROM Member;
+DESC Member;
+
+CREATE TABLE Board
+(
+	id  INT PRIMARY KEY AUTO_INCREMENT,
+   title VARCHAR(255) NOT NULL,
+    body VARCHAR(2047) NOT NULL,
+    memberId VARCHAR(255) NOT NULL,
+    inserted TIMESTAMP DEFAULT NOW()
+);
+
+DESC Board;
+SELECT * FROM Board;
+SELECT * FROM Member;
+
+ALTER TABLE Board
+ADD FOREIGN KEY(MemberId)
+REFERENCES Member(id);
